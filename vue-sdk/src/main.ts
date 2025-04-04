6d8ecab946542aca16e7215beec88e93c370ecc0
tsutsu <tsutsu0928@gmail.com>
2025-04-04 20:13:13 +0900

    Vueのエントリーポイントを改造

--- vue-sdk/src/main.ts
+++ vue-sdk/src/main.ts
@@ -1,6 +1,23 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+type Props = {
+  enableFoo: boolean
+  enableBar: boolean
+  enableBaz: boolean
+}
+
+function parseBoolean(value: string | undefined): boolean {
+  return value === 'true'
+}
+
+document.querySelectorAll<HTMLElement>('#vue-sdk').forEach((el) => {
+  const props: Props = {
+    enableFoo: parseBoolean(el.dataset.enableFoo),
+    enableBar: parseBoolean(el.dataset.enableBar),
+    enableBaz: parseBoolean(el.dataset.enableBaz)
+  }
+  createApp({
+    render: () => h(App, props)
+  }).mount(el)
+})
