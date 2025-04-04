d0cce062b91ce5f59a516766d896e9830ceded47
tsutsu <tsutsu0928@gmail.com>
2025-04-04 20:39:18 +0900

    SDKを使用するサンプルを実装

--- vue-sdk/src/App.vue
+++ vue-sdk/src/App.vue
@@ -2,10 +2,22 @@
 import { ref } from "vue";
 
 const title = ref("This is Vue SDK!!")
+
+defineProps({
+  enableFoo: Boolean,
+  enableBar: Boolean,
+  enableBaz: Boolean
+})
 </script>
 
 <template>
   <h1>{{ title }}</h1>
+
+  <div>
+    <p v-if="enableFoo">Foo 機能が有効</p>
+    <p v-if="enableBar">Bar 機能が有効</p>
+    <p v-if="enableBaz">Baz 機能が有効</p>
+  </div>
 </template>
 
 <style scoped></style>
--- vue-sdk/vite.config.ts
+++ vue-sdk/vite.config.ts
@@ -15,4 +15,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/main.js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
 })
