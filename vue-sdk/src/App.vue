c8034f4b84d0f0c599e9053522b5ccb48f922876
tsutsu <tsutsu0928@gmail.com>
2025-04-04 19:54:59 +0900

    Vueの初期テンプレートを作成

---
+++ vue-sdk/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const title = ref("This is Vue SDK!!")
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped></style>
